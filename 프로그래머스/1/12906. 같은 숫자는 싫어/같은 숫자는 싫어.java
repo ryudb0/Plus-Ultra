da40fe16671453c9e2eb57960afedd135244de32
ryudb0 <122335103+ryudb0@users.noreply.github.com>
2024-09-01 10:35:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.51 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] == arr[i]) {
+                continue;
+            }
+            list.add(arr[i]);
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
