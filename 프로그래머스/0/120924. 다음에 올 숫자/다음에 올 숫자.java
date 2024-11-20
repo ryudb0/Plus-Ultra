997f2e28ebb101958104e470c2ca0740b823a333
ryudb0 <122335103+ryudb0@users.noreply.github.com>
2024-11-20 14:20:59 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] common) {
+        int n = common.length;
+
+        if (common[2] - common[1] == common[1] - common[0]) {
+            return common[n - 1] + (common[n - 1] - common[n - 2]);
+        }
+        return common[n - 1] * (common[n - 1] / common[n - 2]);
+    }
+}
